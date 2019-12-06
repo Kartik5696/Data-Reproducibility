d524ceeb8d3731a611dad3821ecc890e2528b5fb
Kartik5696 <57925882+Kartik5696@users.noreply.github.com>
2019-12-06 16:23:59 -0500

    Import dataset and save file
    Load libraries pandas and matplotlib
    dataset name : survey (1).csv

---
+++ data1202_assignment4.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Load libraries
+import pandas as pd 
+import matplotlib 
+
+
+# In[3]:
+
+
+#Import data
+raw_df = pd.read_csv("survey (1).csv")
+
+
+# In[4]:
+
+
+#Print data
+raw_df.head()
+
+
+# In[ ]:
+
+
+
+
